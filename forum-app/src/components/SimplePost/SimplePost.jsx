38f051dee52cf6d4c59936a6059277d67c500ece
Martin Valov <maritn.valov98@gmail.com>
2024-02-13 01:03:35 +0200

    implementing home page functionality

---
+++ forum-app/src/components/SimplePost/SimplePost.jsx
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+import { useContext } from 'react';
+import { AuthContext } from '../../context/AuthContext';
+
+const SimplePost = ({ postId, postData }) => {
+    const {userData} = useContext(AuthContext);
+
+    return (
+        <div id={postId}>
+            <h3><b>{postData.title}</b></h3>
+            <p>{postData.description}</p>
+            <p>{postData.createdOn}</p>
+            <p>{postData.author}</p>
+            {userData && <button>Like</button>}
+            {/* {userData && <button>Comment</button>} Move to Single view*/}
+            <button>More</button>
+            {userData.username === postData.author && (
+                <>
+                    <button>Edit</button>
+                    <button>Delete</button>
+                </>
+            )}
+        </div>
+    )
+}
+
+SimplePost.propTypes = {
+    postId: PropTypes.string.isRequired,
+    postData: PropTypes.object.isRequired,
+}
+
+export default SimplePost;
--- forum-app/src/views/Home.jsx
+++ forum-app/src/views/Home.jsx
@@ -1,7 +1,23 @@
+import { useEffect, useState } from "react";
+import SimplePost from "../components/SimplePost/SimplePost";
+import { getAllPosts } from "../services/post.service";
+
 export default function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    getAllPosts().then(snapshot => setPosts(Object.entries(snapshot.val())));
+  }, []);
+
   return (
     <div>
       <h1>Home</h1>
+      <div className="all-posts">
+        {posts.map(post => {
+          const [postId, postData] = post
+          return <SimplePost key={postId} postId={postId} postData={postData} />
+        })}
+      </div>
     </div>
   );
 }
