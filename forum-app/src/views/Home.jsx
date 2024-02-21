7c341994e533e3af8c90885a9706bfcb8f960868
Martin Valov <maritn.valov98@gmail.com>
2024-02-21 03:28:16 +0200

    implementing 10 most commented and 10 newest posts

--- forum-app/src/views/Home.jsx
+++ forum-app/src/views/Home.jsx
@@ -21,24 +21,24 @@ export default function Home({ updateUserData }) {
   }, []);
 
   return (
-    <Box>
+    <Box className>
       <Container maxW="100%">
         <Box bgImage="https://c4.wallpaperflare.com/wallpaper/442/68/841/ayrton-senna-helmet-gloves-formula-1-mclaren-mp4-hd-wallpaper-preview.jpg"
-            bgSize={'cover'}
-            w={'99%'}
-            bgPosition="center"
-            color="white"
-            textAlign="center"
-            mb={'2rem'}
-            py={20}>
-        <Heading textAlign={'center'} mb={3} style={{
-                WebkitTextStroke: '1px black',
-                textStroke: '1px black',
-            }}>Welcome to Team 5&apos;s Apex Assembly</Heading>
-        <Heading textAlign={'center'} size={'md'} style={{
-                WebkitTextStroke: '1px black',
-                textStroke: '1px black',
-            }}>A forum about everything Formula 1.</Heading>
+          bgSize={'cover'}
+          w={'99%'}
+          bgPosition="center"
+          color="white"
+          textAlign="center"
+          mb={'2rem'}
+          py={20}>
+          <Heading textAlign={'center'} mb={3} style={{
+            WebkitTextStroke: '1px black',
+            textStroke: '1px black',
+          }}>Welcome to Team 5&apos;s Apex Assembly</Heading>
+          <Heading textAlign={'center'} size={'md'} style={{
+            WebkitTextStroke: '1px black',
+            textStroke: '1px black',
+          }}>A forum about everything Formula 1.</Heading>
         </Box>
         <Flex mb={8} mt={8}>
           <Box p={4} bgColor={"gray.300"} borderRadius="md" flex="1" mx={4}>
@@ -50,11 +50,25 @@ export default function Home({ updateUserData }) {
             <Text fontSize="xl" textAlign={'center'} color={'black'}>{totalPosts}</Text>
           </Box>
         </Flex>
-        <Flex direction={'column'} mx={4} align={"center"}>
-          {posts.map(post => {
-            const [postId, postData] = post
-            return <SimplePost key={post} updateUserData={updateUserData} postId={postId} postData={postData} posts={posts} setPosts={setPosts} />
-          })}
+        <Flex direction={'row'} justify={'center'}>
+          <Flex direction={'column'} p={4}>
+            <Heading size="md" textAlign={'center'} p={4}>Most commented posts</Heading>
+            {posts.filter(post => post[1].comments)
+              .sort((postA, postB) => Object.keys(postB[1].comments).length - Object.keys(postA[1].comments).length)
+              .slice(0, 11).map(post => {
+                const [postId, postData] = post
+                return <SimplePost key={post} updateUserData={updateUserData} postId={postId} postData={postData} posts={posts} setPosts={setPosts} />
+              })}
+          </Flex>
+          <Flex direction={'column'} p={4}>
+            <Heading size="md" textAlign={'center'} p={4}>Newest posts</Heading>
+            {posts.sort((postA, postB) => postB[1].createdOn - postA[1].createdOn)
+              .slice(0, 11)
+              .map(post => {
+                const [postId, postData] = post
+                return <SimplePost key={post} updateUserData={updateUserData} postId={postId} postData={postData} posts={posts} setPosts={setPosts} />
+              })}
+          </Flex>
         </Flex>
       </Container>
     </Box>
